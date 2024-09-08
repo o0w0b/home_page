61f080daa3b26cee6005f6511e6ff3208c13824d
o0w0b <2362760448@qq.com>
2024-09-08 23:50:35 +0800

    update

---
+++ js/es-define-property.7d97634f.js
@@ -0,0 +1,1 @@
+import{g as r}from"./get-intrinsic.1433df2a.js";var t,e;function i(){if(e)return t;e=1;var i=r("%Object.defineProperty%",!0)||!1;if(i)try{i({},"a",{value:1})}catch(a){i=!1}return t=i}export{i as r};
